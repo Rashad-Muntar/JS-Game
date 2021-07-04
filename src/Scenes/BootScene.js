7f8587d639b00f0b916a13149767555a958bf011
Rashad-Muntar <muntartoure@gmail.com>
2021-07-04 12:25:15 +0000

    save score to local storage

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -1,16 +1,11 @@
-import 'phaser';
+import Phaser from 'phaser';
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
     super('Boot');
   }
 
-  preload() {
-    this.load.image('logo', 'assets/logo.png');
-  }
-
   create() {
     this.scene.start('Preloader');
-    this.add.image(50, 50, 'alien');
   }
 }
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -3,14 +3,34 @@ import 'phaser';
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
+    
   }
 
-  preload() {
-    // load images
+  preload(){
     this.load.image("platform", "platform.png");
-  }
 
-  // create() {
-  //   this.add.image(400, 300, 'log');
-  // }
-}
+    // player is a sprite sheet made by 24x48 pixels
+    this.load.spritesheet("player", "player.gif", {
+        frameWidth: 24,
+        frameHeight: 48
+    })
+
+    this.add.spritesheet(0, 0, 'platform')
+
+    // the coin is a sprite sheet made by 20x20 pixels
+    this.load.spritesheet("coin", "coin.png", {
+        frameWidth: 20,
+        frameHeight: 20
+    })
+
+    // // mountains are a sprite sheet made by 512x512 pixels
+    // this.load.spritesheet("mountain", "mountain.png", {
+    //     frameWidth: 512,
+    //     frameHeight: 512
+    // });
+  }
+  create() {
+    this.add.image(400, 300, 'log');
+  }
+ }
+
