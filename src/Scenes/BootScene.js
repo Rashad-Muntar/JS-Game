932c03c568c6b2aec83db001289037ccf3b84b53
Rashad-Muntar <muntartoure@gmail.com>
2021-07-08 00:59:24 +0000

    add player platform collition

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -9,7 +9,10 @@ export default class BootScene extends Phaser.Scene {
     this.load.image('sky', 'assets/sky.png')
     this.load.image('logo', 'assets/zenva_logo.png');
     this.load.image('ground', 'assets/platform.png')
-
+    this.load.spritesheet('dude', 'assets/dude.png', {
+      frameWidth: 32, frameHeight: 48
+  })
+  
   }
 
   create () {
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,6 +1,7 @@
 import 'phaser';
 
 let platforms
+let player
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super('Game');
@@ -20,6 +21,34 @@ export default class GameScene extends Phaser.Scene {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude')
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+  });
+  
+  this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+  });
+  
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+  player.body.setGravityY(300)
+  this.physics.add.collider(player, platforms)
   }
 
 };
